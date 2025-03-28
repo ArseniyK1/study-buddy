992a0cad81b7083d44f23b2e309e323a2e661cf2
ArseniyK1 <qwertototo2332@gmail.com>
2025-03-28 08:35:46 +0300

    f

--- back/grpc/generated-types.bat
+++ back/grpc/generated-types.bat
@@ -1,5 +1,5 @@
 protoc ^
-  --plugin=protoc-gen-ts_proto=D:\development\projects_git\study-buddy\back\grpc\node_modules\.bin\protoc-gen-ts_proto.cmd ^
+  --plugin=protoc-gen-ts_proto=node_modules\.bin\protoc-gen-ts_proto.cmd ^
   --ts_proto_out=./src/proto/generated ^
   --ts_proto_opt=outputServices=grpc-js,nestJs=true,addGrpcMetadata=true,addNestjsRestParameter=true,returnObservable=false ^
   -I=./src/proto ^
