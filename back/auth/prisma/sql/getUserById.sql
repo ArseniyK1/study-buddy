3af1427a822eabff8260d69c4453e08b22f67dd7
Arseniy Kiselev <akiselev@medsoft.su>
2025-04-03 11:12:49 +0300

    fix: перенос файлов

--- back/auth/prisma/sql/getUserById.sql
+++
@@ -1,4 +0,0 @@
-SELECT a.*
-FROM auth_user a
-WHERE a.id = $1
---
--- back/auth/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthUserModule } from './auth-user/auth-user.module';
-
-@Module({
-  imports: [AuthUserModule],
-})
-export class AppModule {}
